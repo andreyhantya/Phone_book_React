a203c1f925e1d15a652224ca8e6bff900f097cfd
Andrey <andreyhantya@gmail.com>
2021-01-26 00:59:37 +0200

    Changed styles

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './app.css'
 import NavigationPanel from "../navigation-panel/navigation-panel";
 import BookList from "../book-list/book-list";
+import ContactCounter from "../contact-counter/contact-counter";
 
 
 export default class App extends Component{
@@ -9,14 +10,12 @@ export default class App extends Component{
     maxId = 100;
 
     state = {
-        contacts : [
-            {name: 'Andre', number : '+380665475602', id : '1'},
-            {name: 'Vasya', number : '+777888777777', id : '2'}
-        ]
+        contacts : [],
+        contactCounter : 0
     }
 
     addContact = (name, number) => {
-        const {contacts} = this.state;
+        const {contacts, contactCounter} = this.state;
 
         const newContact = {
             name: name,
@@ -24,24 +23,42 @@ export default class App extends Component{
             id: this.maxId++
         }
 
+        this.contactCounter(contactCounter.length);
+
         this.setState(() => {
             return {
                 contacts:[ ...contacts,
                             newContact
-                ]
+                ],
+                contactCounter : contactCounter + 1
             }
         })
 
     };
 
+    contactCounter = (count) => {
+        return this.setState({contactCounter : count - 1})
+    }
 
+    deleteContact = (id) => {
+        const {contacts, contactCounter} = this.state;
+        const idx = this.state.contacts.findIndex(el => el.id === id);
+
+        this.contactCounter(contactCounter.length);
+        this.setState({
+            contacts: [...contacts.slice(0, idx),
+                    ...contacts.slice(idx+1)],
+            contactCounter: contactCounter-1
+        })
+    }
 
 
     render() {
         return (
             <div className='phone-book__wrapper'>
                 <NavigationPanel addContact={(name, number) => this.addContact(name, number)}/>
-                <BookList contacts={this.state.contacts} />
+               <ContactCounter contactCounter={this.state.contactCounter}/>
+                <BookList contacts={this.state.contacts} deleteContact={this.deleteContact} />
             </div>
         );
     }
